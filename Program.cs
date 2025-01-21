a0ef60228089ab98bd81173c2f60db47c689538d
Kasper Olesen <kaspersolesen@gmail.com>
2025-01-21 18:17:48 +0100

    Delete Program.cs

--- Program.cs
+++
@@ -1,5 +0,0 @@
-        public static async Task Main(string[] args)
-        {
-            var builder = WebApplication.CreateBuilder(args);
-            builder.Services.AddSingleton<BenchmarkService>();
-            builder.Services.AddTransient<BenchmarkTests>();
